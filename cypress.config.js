a807a53e73421dec9f2930b6b2528eb1a7e57c4e
Nandish-P <72277827+Nandu890@users.noreply.github.com>
2023-03-01 19:22:35 +0530

    Updated

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,8 @@ module.exports = defineConfig({
     baseUrl:"https://demoqa.com",
     specPattern:"cypress/e2e/integration/*.{js,jsx,ts,tsx}",
     chromeWebSecurity: false,
+    video:false,
+    videoCompression:false
     
   },
   viewportHeight:840,
